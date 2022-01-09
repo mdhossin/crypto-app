a890de71f285e92831e1cb595d7af8cdbbf274f6
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-01-09 15:31:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Space, Typography } from "antd";
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import {
+  Navbar,
+  Homepage,
+  Cryptocurriences,
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  News,
+  CryptoDetail,
+} from "./components";
+
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
     </div>
-  );
-}
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+      
+            <Route
+              path="/cryptocurrenices"
+              element={<Cryptocurriences />}
+            ></Route>
+            <Route path="/crypto/:coinId" element={<CryptoDetail />}></Route>
+            <Route path="/news" element={<News />}></Route>
+          </Routes>
+        </div>
+      </Layout>
+ 
+    <div className="footer">
+      <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+    </div>
+    </div>
+  </div>
+);
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar";
+export {default as Homepage} from './Homepage'
+export {default as Cryptocurriences} from './Cryptocurriences'
+export {default as CryptoDetail} from './CryptoDetail'
+export {default as News} from './News'
+
